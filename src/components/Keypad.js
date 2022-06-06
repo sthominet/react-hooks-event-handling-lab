35e278c7b1e99fa4f36d9b99d865cbf3c5bdf46e
Sandrine <sandrine.thominet@gmail.com>
2022-06-05 21:42:27 -0400

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad () {
+
+    function password() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input onChange={password}type="password" />
+    );
+}
+
+export default Keypad;
